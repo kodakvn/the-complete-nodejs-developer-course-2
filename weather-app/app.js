8d3bdf94b80d2df6f4719fdc7bcd1897ff72f5c9
kodakvn <linhtt.it@gmail.com>
2021-01-30 23:41:27 +0700

    31 - Encoding User Input with challenge

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,7 +1,24 @@
 const request = require('request');
+const { alias } = require('yargs');
+const yargs = require('yargs');
+
+const argv = yargs
+    .options({
+        a: {
+            demand: true,
+            alias: 'address',
+            describe: 'Address to fetch weather for',
+            string: true
+        }
+    })
+    .help()
+    .alias('help', 'h')
+    .argv;
+
+console.log(argv);
 
 request({
-    url: 'http://api.positionstack.com/v1/forward?access_key=31becd37dacff8d8b87369f38bee0e51&query=1600%20Pennsylvania%20Ave%20NW,%20Washington%20DC',
+    url: `http://api.positionstack.com/v1/forward?access_key=31becd37dacff8d8b87369f38bee0e51&query=${encodeURIComponent(argv.a)}`,
     json: true
 }, (error, response, body) => {
     console.log(`Address: ${body.data[0].name}`);
