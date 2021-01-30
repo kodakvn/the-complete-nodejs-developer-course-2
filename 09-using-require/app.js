a843e48c943062165f288d6bfbd7015ad2426877
kodakvn <linhtt.it@gmail.com>
2021-01-30 22:20:40 +0700

    10 - require own files: export & call function

--- 09-using-require/app.js
+++ 09-using-require/app.js
@@ -7,11 +7,15 @@ const notes = require('./notes.js');
 var userInfo = os.userInfo();
 
 // #1
-fs.appendFile('greetings.txt', `hello ${userInfo.username}! You are ${notes.age}`, function (err) {
-    if (err) {
-        console.log('unable to write file');
-    }
-});
+// fs.appendFile('greetings.txt', `hello ${userInfo.username}! You are ${notes.age}`, function (err) {
+//     if (err) {
+//         console.log('unable to write file');
+//     }
+// });
 
 // #2
 // fs.appendFileSync('greetings.txt', 'hello world new');
+
+// #10 - export function
+var res = notes.addNode();
+console.log(res);
--- 09-using-require/notes.js
+++ 09-using-require/notes.js
@@ -1,3 +1,7 @@
 console.log('starting node.js');
 
-module.exports.age = 25;
+// module.exports.age = 25;
+module.exports.addNode = () => {
+    console.log('add note');
+    return 'new note';
+};
