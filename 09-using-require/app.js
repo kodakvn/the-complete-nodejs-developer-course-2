8fb171ec79d8f3d3b27c47e4119a781c1c09bf65
kodakvn <linhtt.it@gmail.com>
2021-01-30 22:21:35 +0700

    12 install nodemon

--- 09-using-require/app.js
+++ 09-using-require/app.js
@@ -1,11 +1,5 @@
 console.log('starting app.js');
 
 const fs = require('fs');
-const os = require('os');
 const _ = require('lodash');
 const notes = require('./notes.js');
-
-// console.log(_.isString(true));
-// console.log(_.isString('kodakvn'));
-var filterredArray = _.uniq(['kodak', 1, 'kodak', 1, 2, 3, 4]);
-console.log(filterredArray);
