5929aec6ce6331b2316e5e585eceeb7597e8c69e
kodakvn <linhtt.it@gmail.com>
2021-01-30 22:22:47 +0700

    15 - json challenge: using stringify & parse

--- 09-using-require/playground/json.js
+++ 09-using-require/playground/json.js
@@ -1,3 +1,4 @@
+// #1: object to json string
 // var obj = {
 //     name: 'kodak'
 // };
@@ -6,7 +7,29 @@
 // console.log(typeof stringObj);
 // console.log(stringObj);
 
-var personString = '{"name": "kodak", "age": 25}';
-var person = JSON.parse(personString);
-console.log(typeof person);
-console.log(person);
+// #2: json string to object
+// var personString = '{"name": "kodak", "age": 25}';
+// var person = JSON.parse(personString);
+// console.log(typeof person);
+// console.log(person);
+
+// #3: challenge
+const fs = require('fs');
+
+var originalNote = {
+    title: 'some title',
+    body: 'some body'
+};
+
+// #3.1: stringify
+var originalNoteString = JSON.stringify(originalNote);
+
+fs.writeFileSync('note.json', originalNoteString);
+
+var noteString = fs.readFileSync('note.json');
+
+// #3.2: parse
+var note = JSON.parse(noteString);
+
+console.log(typeof note);
+console.log(note.title);
