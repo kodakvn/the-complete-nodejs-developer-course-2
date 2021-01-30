2238cd43dfea7fe356a4608ebe1982ff4b59e8b4
kodakvn <linhtt.it@gmail.com>
2021-01-30 22:21:53 +0700

    13 - get user inputs

--- 09-using-require/app.js
+++ 09-using-require/app.js
@@ -3,3 +3,19 @@ console.log('starting app.js');
 const fs = require('fs');
 const _ = require('lodash');
 const notes = require('./notes.js');
+
+var command = process.argv[2];
+console.log('Command: ', command);
+console.log(process.argv);
+
+if (command == 'add') {
+    console.log('adding new note');
+} else if (command == 'list') {
+    console.log('listing all notes')
+} else if (command == 'read') {
+    console.log('read a note')
+} else if (command == 'remove') {
+    console.log('remove notes')
+} else {
+    console.log('Command not recognized');
+}
