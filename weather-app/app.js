d7baee7a9fd9e0c8c429fb2efeb90d3db33d4a56
kodakvn <linhtt.it@gmail.com>
2021-01-30 23:19:02 +0700

    29 - Pretty Printing Objects

--- weather-app/app.js
+++ weather-app/app.js
@@ -4,5 +4,5 @@ request({
     url: 'http://api.positionstack.com/v1/forward?access_key=31becd37dacff8d8b87369f38bee0e51&query=1600%20Pennsylvania%20Ave%20NW,%20Washington%20DC',
     json: true
 }, (error, response, body) => {
-    console.log(body);
+    console.log(JSON.stringify(body, undefined, 2));
 });
