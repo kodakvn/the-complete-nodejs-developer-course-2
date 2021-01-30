7ec38dc204a47daa1adbe53366337547f59760ab
kodakvn <linhtt.it@gmail.com>
2021-01-30 22:24:17 +0700

    23 - Advanced Yargs

--- 09-using-require/app.js
+++ 09-using-require/app.js
@@ -2,11 +2,37 @@ console.log('starting app.js');
 
 const fs = require('fs');
 const _ = require('lodash');
+const { demand } = require('yargs');
 const yargs = require('yargs');
 
 const notes = require('./notes.js');
 
-const argv = yargs.argv;
+const titleDescription = {
+    describe: 'Title of note',
+    demand: true,
+    alias: 't'
+}
+
+const bodyDescription = {
+    describe: 'Body of note',
+    demand: true,
+    alias: 'b'
+}
+
+const argv = yargs
+    .command('add', 'Add a new note', {
+        title: titleDescription,
+        body: bodyDescription
+    })
+    .command('list', 'List all notes')
+    .command('read', 'Read a note', {
+        title: titleDescription
+    })
+    .command('remove', 'Remove a note', {
+        title: titleDescription
+    })
+    .help()
+    .argv;
 var command = argv._[0];
 
 if (command == 'add') {
