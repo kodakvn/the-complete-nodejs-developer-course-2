07f23705e9d5c19fb9c68947b13ca39c598cbb07
kodakvn <linhtt.it@gmail.com>
2021-01-30 22:22:33 +0700

    15 - json parse

--- 09-using-require/playground/json.js
+++ 09-using-require/playground/json.js
@@ -1,7 +1,12 @@
-var obj = {
-    name: 'kodak'
-};
+// var obj = {
+//     name: 'kodak'
+// };
 
-var stringObj = JSON.stringify(obj);
-console.log(typeof stringObj);
-console.log(stringObj);
+// var stringObj = JSON.stringify(obj);
+// console.log(typeof stringObj);
+// console.log(stringObj);
+
+var personString = '{"name": "kodak", "age": 25}';
+var person = JSON.parse(personString);
+console.log(typeof person);
+console.log(person);
