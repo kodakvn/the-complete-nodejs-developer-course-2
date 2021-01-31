e9622f0a12412eedb37cc852029fd1914977a9ab
kodakvn <linhtt.it@gmail.com>
2021-01-31 22:57:41 +0700

    34 - Wiring Up Weather Search

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,25 +1,38 @@
-const { alias } = require('yargs');
-const yargs = require('yargs');
+// const { alias } = require('yargs');
+// const yargs = require('yargs');
 
-const geocode = require('./geocode/geocode');
+// const geocode = require('./geocode/geocode');
 
-const argv = yargs
-    .options({
-        a: {
-            demand: true,
-            alias: 'address',
-            describe: 'Address to fetch weather for',
-            string: true
-        }
-    })
-    .help()
-    .alias('help', 'h')
-    .argv;
+// const argv = yargs
+//     .options({
+//         a: {
+//             demand: true,
+//             alias: 'address',
+//             describe: 'Address to fetch weather for',
+//             string: true
+//         }
+//     })
+//     .help()
+//     .alias('help', 'h')
+//     .argv;
 
-geocode.geocodeAddress(argv.a, (error, results) => {
-    if (error) {
-        console.log(error);
+// geocode.geocodeAddress(argv.a, (error, results) => {
+//     if (error) {
+//         console.log(error);
+//     } else {
+//         console.log(JSON.stringify(results, undefined, 2));
+//     }
+// });
+
+const request = require('request');
+
+request({
+    url: 'http://api.openweathermap.org/data/2.5/weather?lat=48.180532&lon=11.420413&appid=d80184a6000ab237f2597638ad1e725c',
+    json: true
+}, (error, response, body) => {
+    if (!error && response.statusCode === 200) {
+        console.log(body.main.temp);
     } else {
-        console.log(JSON.stringify(results, undefined, 2));
+        console.log('unable to fetch weather');
     }
 });
