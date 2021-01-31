015d8c4e7a6c7a7aeacad1fa4033ae4a33b844c4
kodakvn <linhtt.it@gmail.com>
2021-01-31 22:16:54 +0700

    32 - Callback Errors

--- weather-app/app.js
+++ weather-app/app.js
@@ -21,7 +21,13 @@ request({
     url: `http://api.positionstack.com/v1/forward?access_key=31becd37dacff8d8b87369f38bee0e51&query=${encodeURIComponent(argv.a)}`,
     json: true
 }, (error, response, body) => {
-    console.log(`Address: ${body.data[0].name}`);
-    console.log(`Latitude: ${body.data[0].latitude}`);
-    console.log(`Longitude: ${body.data[0].longitude}`);
+    if (error) {
+        console.log('unable to connect to api');
+    } else if (body.data.length == 0) {
+        console.log('unable to find address');
+    } else {
+        console.log(`Address: ${body.data[0].name}`);
+        console.log(`Latitude: ${body.data[0].latitude}`);
+        console.log(`Longitude: ${body.data[0].longitude}`);
+    }
 });
