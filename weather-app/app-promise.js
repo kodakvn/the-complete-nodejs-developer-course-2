35a1b456304902daf9f8dd2664a898285f974620
kodakvn <linhtt.it@gmail.com>
2021-02-01 09:51:17 +0700

    ignore api key

--- weather-app/app-promise.js
+++ weather-app/app-promise.js
@@ -1,5 +1,6 @@
 const yargs = require('yargs');
 const axios = require('axios');
+const fs = require('axios');
 
 const argv = yargs
     .options({
@@ -14,8 +15,12 @@ const argv = yargs
     .alias('help', 'h')
     .argv;
 
+// TODO - read from file, ignored this file
+var geocodeApiKey = ''; 
+var weatherApiKey = '';
+
 var encodedAddress = encodeURIComponent(argv.a);
-var geocodeUrl = `http://api.positionstack.com/v1/forward?access_key=31becd37dacff8d8b87369f38bee0e51&query=${encodedAddress}`;
+var geocodeUrl = `http://api.positionstack.com/v1/forward?access_key=${geocodeApiKey}&query=${encodedAddress}`;
 
 axios.get(geocodeUrl).then((response) => {
     if (response.data.length === 0) {
@@ -24,7 +29,7 @@ axios.get(geocodeUrl).then((response) => {
     
     var lat = response.data.data[0].latitude;
     var lon = response.data.data[0].longitude;
-    var weatherUrl = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=d80184a6000ab237f2597638ad1e725c`;
+    var weatherUrl = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${weatherApiKey}`;
     console.log(response.data.data[0].name);
     return axios.get(weatherUrl);
 }).then((response) => {
