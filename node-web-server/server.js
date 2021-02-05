15e9800b81e2fda903c2562afcbbd0ac01217276
kodakvn <linhtt.it@gmail.com>
2021-02-05 23:37:33 +0700

    43 - add HandleBar, render templates with data for home & about pages

--- node-web-server/server.js
+++ node-web-server/server.js
@@ -1,22 +1,24 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
+app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
-    // res.send('<h1>hello express</h1>');
-    res.send({
-        name: 'kodak',
-        likes: [
-            'biking',
-            'cities'
-        ]
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        welcomeMessage: 'Welcome to my website',
+        currentYear: new Date().getFullYear()
     });
 });
 
 app.get('/about', (req, res) => {
-    res.send('about page');
+    res.render('about.hbs', {
+        pageTitle: 'About Page',
+        currentYear: new Date().getFullYear()
+    });
 });
 
 // /bad - send back json with error message
